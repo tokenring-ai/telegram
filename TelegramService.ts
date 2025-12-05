d4087ced7af7721ecdac9ebb33f02d3d8942bf1d
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-04 19:09:29 -0800

    Refactored event handling to simplify access to event properties and improve readability

--- TelegramService.ts
+++ TelegramService.ts
@@ -71,13 +71,13 @@ export default class TelegramService implements TokenRingService {
           for (const event of state.yieldEventsByCursor(eventCursor)) {
             switch (event.type) {
               case 'output.chat':
-                this.handleChatOutput(chatId, event.data.content);
+                this.handleChatOutput(chatId, event.content);
                 break;
               case 'output.system':
-                this.handleSystemOutput(chatId, event.data.message, event.data.level);
+                this.handleSystemOutput(chatId, event.message, event.level);
                 break;
               case 'input.handled':
-                if (event.data.requestId === requestId) {
+                if (event.requestId === requestId) {
                   unsubscribe();
                   // If no response was sent, send a default message
                   if (!this.lastResponseSent) {
