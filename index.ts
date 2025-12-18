e63af1e0d0d4e5d434c22852690d4cb819fe35af
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:39 -0800

    Extract and modularize plugin logic into `plugin.ts`

--- index.ts
+++ index.ts
@@ -1,19 +1,1 @@
-import TokenRingApp from "@tokenring-ai/app";
-import {TokenRingPlugin} from "@tokenring-ai/app";
-import packageJSON from './package.json' with {type: 'json'};
-import TelegramService, {TelegramServiceConfigSchema} from "./TelegramService.ts";
-
-export default {
-  name: packageJSON.name,
-  version: packageJSON.version,
-  description: packageJSON.description,
-  install(app: TokenRingApp) {
-    const telegramConfig = app.getConfigSlice("telegram", TelegramServiceConfigSchema.optional());
-
-    if (telegramConfig) {
-      app.addServices(new TelegramService(app, telegramConfig));
-    }
-  },
-} as TokenRingPlugin;
-
 export {default as TelegramBotService} from "./TelegramService.ts";
