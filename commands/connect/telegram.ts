769b6fed221751f5263dc6d946ba881eac3ba95c
Mark Dierolf <mdierolf@financialcontent.com>
2026-08-06 15:21:27 -0700

    Update Telegram plugin: change `save` configuration options to `global` and `workspace` with default `workspace`

--- commands/connect/telegram.ts
+++ commands/connect/telegram.ts
@@ -12,8 +12,8 @@ const inputSchema = {
     save: {
       description: "Where to save the Telegram account configuration",
       type: "enum",
-      values: ["user", "project"],
-      defaultValue: "user",
+      values: ["global", "workspace"],
+      defaultValue: "workspace",
     },
   },
   positionals: [
